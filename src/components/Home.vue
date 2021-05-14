dab9611946d20b996b7bed4a515d1120210818e5
Prashanthi Ramesh <rvprashanthi@gmail.com>
2021-05-14 09:02:36 -0400

    added vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h4 class="font-weight-bold mb-3 heading">
+          We are building something awesome for you! :)
+        </h4>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+  }
+</script>
+
+<style scoped>
+  .heading {
+    font-family: cursive;
+    color: grey;
+  }
+</style>
